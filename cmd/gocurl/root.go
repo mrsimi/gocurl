036e40133410cebf05b74fdb0773afc3dccfef4f
simi <adegokesimi@gmail.com>
2023-08-07 00:03:41 +0100

    get requests with authorization

--- cmd/gocurl/root.go
+++ cmd/gocurl/root.go
@@ -9,6 +9,8 @@ import (
 )
 
 var url string
+var headers []string
+
 var rootCmd = &cobra.Command{
 	Use: "gocurl [request type]",
 	Short: "gocurl - a go version of the curl tool",
@@ -17,19 +19,16 @@ it is command-line tool for getting or sending data including files using URL sy
 	Args: cobra.MinimumNArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
-		switch requestType := args[0]; requestType{
-		case "GET", "get":
-			result := gocurl.GetRequest(url)
-			fmt.Println(result)
-		default: 
-			fmt.Println("Request Type not supported yet")
-		}
+		requestType := args[0];
+		result := gocurl.ApiRequest(requestType, url, headers)
+		fmt.Println(result)
 	},
 }
 
 func Execute(){
 	//setting all the flags
 	rootCmd.Flags().StringVarP(&url, "url", "u", "", "resource url")
+	rootCmd.Flags().StringSliceVarP(&headers, "headers", "x", []string{}, "")
 
 	if err := rootCmd.Execute(); err != nil{
 		fmt.Fprintln(os.Stderr, "Whopps. There was an error while executing your CLI '%s'", err)
--- pkg/gocurl/gocurl.go
+++ pkg/gocurl/gocurl.go
@@ -1,14 +1,28 @@
 package gocurl
 
-import(
+import (
 	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 )
 
-func GetRequest(url string)(response string){
-	resp, err := http.Get(url)
+func ApiRequest(method string, url string, headers []string)(response string){
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(strings.ToUpper(method), url, nil)
+	if err != nil{
+		return err.Error()
+	}
+
+	for _, headerContent:= range headers{
+		headerContentSplit := strings.Split(headerContent, ":")
+		req.Header.Add(headerContentSplit[0], headerContentSplit[1])
+	}
+	
+	resp, err := client.Do(req)
 	if err != nil{
 		return err.Error()
 	}
@@ -16,6 +30,10 @@ func GetRequest(url string)(response string){
 	body, err := io.ReadAll(resp.Body)
 	if err != nil{
 		return err.Error()
+	}
+
+	if len(body) == 0{
+		return resp.Status
 	}
 
 	var prettyJSON bytes.Buffer
